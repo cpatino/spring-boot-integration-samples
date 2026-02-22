7eec1babdc99f6ebac7529de39e6b3fd9618e561
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2026-02-22 14:06:51 +0100

    Replace SnsOperation for SnsTemplate in the SnsPublisherService class

--- publisher/src/main/java/com/carp/sample/publisher/service/SnsPublisherService.java
+++ publisher/src/main/java/com/carp/sample/publisher/service/SnsPublisherService.java
@@ -1,8 +1,7 @@
 package com.carp.sample.publisher.service;
 
 import com.carp.sample.publisher.exception.MessageNotPublishedException;
-import io.awspring.cloud.sns.core.SnsNotification;
-import io.awspring.cloud.sns.core.SnsOperations;
+import io.awspring.cloud.sns.core.SnsTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
@@ -12,14 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 class SnsPublisherService implements PublisherService {
 
-    private static final String TOPIC_ARN = "arn:aws:sns:eu-west-1:000000000000:my-topic";
+    private static final String TOPIC_NAME = "my-topic";
     private static final String MECHANISM = "BROADCAST";
     private static final Logger log = LoggerFactory.getLogger(SnsPublisherService.class);
 
-    private final SnsOperations snsOperations;
+    private final SnsTemplate snsTemplate;
 
-    SnsPublisherService(SnsOperations snsOperations) {
-        this.snsOperations = snsOperations;
+    SnsPublisherService(SnsTemplate snsTemplate) {
+        this.snsTemplate = snsTemplate;
     }
 
     @Override
@@ -30,8 +29,7 @@ class SnsPublisherService implements PublisherService {
     @Override
     public void publish(String content) {
         try {
-            SnsNotification<String> notification = SnsNotification.builder(content).build();
-            snsOperations.sendNotification(TOPIC_ARN, notification);
+            snsTemplate.sendNotification(TOPIC_NAME, content, null);
         } catch (Exception e) {
             log.error("Failed to publish message to SNS topic: {}", e.getMessage(), e);
             throw new MessageNotPublishedException();
